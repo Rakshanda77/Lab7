9eefba8b4bcf3b7d497164d427eaace38cb37939
Rakshanda <rakshanda92041@gmail.com>
2019-11-28 15:35:02 -0800

    working

--- app/src/main/java/com/example/implicitintent/Brief.java
+++ app/src/main/java/com/example/implicitintent/Brief.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -13,14 +14,16 @@ public class Brief extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_brief);
-        WebView webView = (WebView) findViewById(R.id.webview);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
 
-
+        WebView webview =(WebView)findViewById(R.id.webview);
         Intent intent = getIntent();
-        //String brief = intent.getStringExtra("url");
 
+        String brief = intent.getStringExtra("url");
+        // String brief1 = intent.getStringExtra("result1");
+
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl(brief);
 
 
 
--- app/src/main/java/com/example/implicitintent/MainActivity.java
+++ app/src/main/java/com/example/implicitintent/MainActivity.java
@@ -1,68 +1,58 @@
 package com.example.implicitintent;
-
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
+import android.widget.TextView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
+    static ArrayList Url = new ArrayList<String>();;
+    static  Intent i;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ListView listView = findViewById(R.id.listview);
+       // final TextView textView = findViewById(R.id.textView);
 
-        ListView listView = findViewById(R.id.listview);
-        ArrayList<String> listItems = new ArrayList<String>();
-        ArrayList<String> url = new ArrayList<String>();
+        i = getIntent();
 
-        Intent i = getIntent();
-        String action = i.getAction();
-        String data1 = i.getStringExtra(Intent.EXTRA_TEXT);
+        String data = i.getStringExtra(Intent.EXTRA_TEXT);
 
-        listItems.add(data1);
+        if (data == null) {
+            Url.add("");
+        } else {
+             Url.add( "" + data + "" );
+        }
 
-        i.putExtra("url", data1);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, Url);
 
-//        String type = i.getType();
-//        Uri data = i.getData();
-//        Intent intent = getIntent();
-//        intent.putStringArrayListExtra("test", (ArrayList<String>) listItems);
-//        ArrayList<String> test = getIntent().getStringArrayListExtra("test");
-
-        listItems.add(data1);
-        ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, listItems);
         listView.setAdapter(arrayAdapter);
-
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
 
-                Object position = listView.getItemAtPosition(i);
-                String URL = url.get(i);
+                Object links = listView.getItemAtPosition(i);
+                String link = (String) Url.get(i);
+
+
 
                 Intent intent = new Intent(MainActivity.this, Brief.class);
-               // intent.putExtra("url", URL);
+                intent.putExtra("url", link);
                 startActivity(intent);
-
             }
         });
-    }
 
+    }
 }
